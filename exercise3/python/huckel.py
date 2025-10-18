d11c8b2ca7b72041384251843de7e55c7828f140
zsy-sdu <19862672608@163.com>
2025-10-18 20:23:05 +0800

    update

--- exercise3/python/huckel.py
+++ exercise3/python/huckel.py
@@ -1,7 +1,10 @@
 import numpy as np
-
-H = np.array([[1.0, 1.0], [1.0, 1.0]])
-e, c = np.linalg.eigh(H)
-
-print(e)
-print(c)
+def simple_huckel():
+  H = np.array([[1.0, 1.0], [1.0, 1.0]])
+  e, c = np.linalg.eigh(H)
+  print("\n本征值 (分子轨道能级):")
+  print(e)
+  print("\n本征向量 (分子轨道系数):")
+  print(c)
+if __name__ == "__main__":
+    simple_huckel()
